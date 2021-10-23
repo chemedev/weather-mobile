51e8cb2156fe25ef79ec306f59d413767790e195
Juan E. Chemello <juanchemell@gmail.com>
2021-10-23 14:28:44 -0300

    Se implementa tab navigator en la app

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from "react-native-vector-icons/Ionicons"
+
+import { Home, CitiesList, About } from "./app/screens"
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+            switch (route.name) {
+              case "Inicio":
+                iconName = focused ? "home" : "home-outline"
+                break
+              case "Ciudades":
+                iconName = focused ? "map" : "map-outline"
+                break
+              case "Quiénes Somos":
+                iconName = focused ? "help-circle" : "help-circle-outline"
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray"
+        })}
+      >
+        <Tab.Screen name="Inicio" component={Home} />
+        <Tab.Screen name="Ciudades" component={CitiesList} />
+        <Tab.Screen name="Quiénes Somos" component={About} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
